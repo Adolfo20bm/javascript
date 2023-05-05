93113424ea46fdc67881acfde8b6dd6763cc6e5f
Adolfo20bm <abm2491@gmail.com>
2023-05-05 18:41:48 -0500

    String o cadena de texto

---
+++ js/04.js
@@ -0,0 +1,11 @@
+// String o cadena de texto
+
+const producto = 'MOnitor de 20"'; //Mas comun de usar
+const producto2 = String('Monior 30 Pugadas');
+const producto3 = new String('Monior 30 Pugadas');
+
+//typeof es para saber que tipo de dato es una variable u objeto
+
+console.log(producto)
+console.log(typeof producto2)
+console.log(producto3)
