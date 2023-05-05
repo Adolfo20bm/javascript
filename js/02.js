5db1f4647b25ed8fec234be03668881c0e67008d
Adolfo20bm <abm2491@gmail.com>
2023-05-05 18:31:39 -0500

    variables con let y const

---
+++ js/02.js
@@ -0,0 +1,25 @@
+// Variables 
+
+let producto = 'Audifonos Gamer'; //Inicia variable y asignarle valor
+
+let disponible; // iniciamos la variable pero sin valor
+
+producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+let producto1 = 'Computadora',
+disponible1 = true,
+categoria = 'Computadora';
+
+//let disponibles
+let disponible_;
+
+//Estiulos para las variables
+let nombre_producto = 'Monitor HD'; //underscore
+let nombreProducto = 'Monitor HD'; //Camelcase
+let NombrePproducto = 'Monitor HD'; //Pascal Case
+let nombreproducto = 'MonitorHD'; //underscore
+
+//Moastar por consola la variable
+console.log(producto);
---
+++ js/03.js
@@ -0,0 +1,25 @@
+// Variables const quiere decir que no puedes cambiar
+
+const producto = 'Audifonos Gamer'; //Inicia variable y asignarle valor
+
+// const disponible;  No puedes inicializar sin valor
+
+// producto = true; //Reasignando el valor de la variable
+
+let disponible = true;
+
+const producto1 = 'Computadora',
+disponible1 = true,
+categoria = 'Computadora';
+
+//const disponibles
+const disponible_ = true;
+
+//Estiulos para las variables
+const nombre_producto = 'Monitor HD'; //underscore
+const nombreProducto = 'Monitor HD'; //Camelcase
+const NombrePproducto = 'Monitor HD'; //Pascal Case
+const nombreproducto = 'MonitorHD'; //underscore
+
+//Moastar por consola la variable
+console.log(producto);
