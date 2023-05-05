c8ef0e8970c1f6ce64205cb8dcd3eb5c52e4b8f1
Adolfo20bm <abm2491@gmail.com>
2023-05-05 18:22:33 -0500

    variables en javascript

---
+++ js/01.js
@@ -0,0 +1,26 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; //Inicia variable y asignarle valor
+
+var disponible; // iniciamos la variable pero sin valor
+
+producto = true; //Reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+disponible1 = true,
+categoria = 'Computadora';
+
+//var disponibles
+var disponible_;
+
+//Estiulos para las variables
+var nombre_producto = 'Monitor HD'; //underscore
+var nombreProducto = 'Monitor HD'; //Camelcase
+var NombrePproducto = 'Monitor HD'; //Pascal Case
+var nombreproducto = 'MonitorHD'; //underscore
+
+console.log(producto);
+
+
