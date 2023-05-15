2260ba1db5f286b44caaa6fc6828bf0904d16f75
Adolfo20bm <abm2491@gmail.com>
2023-05-15 09:01:36 -0500

    Objects Methods

---
+++ js/12.js
@@ -0,0 +1,18 @@
+//"use strict"; //correr JS en modo estricto
+
+//Objetos
+const producto = {
+    nombreProducto : "Monitor 20 Pulgadas",
+    precio : 300,
+    disponible : true
+}
+
+Object.seal(producto); // .freeze .seal
+
+producto.precio = 'NUEVO PRECIO';
+
+delete producto.precio;
+
+
+console.log(producto);
+console.log(Object.isFrozen(producto));
