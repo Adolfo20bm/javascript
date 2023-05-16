bc6c4ace0ba759947c3fed84243d58230bfc36b7
Adolfo20bm <abm2491@gmail.com>
2023-05-16 06:53:43 -0500

    Funciones con parametros y argumentos

---
+++ js/18.js
@@ -0,0 +1,12 @@
+function sumar(numero1=0, numero2=0) {
+    console.log(numero2+numero1);
+}
+sumar(10,5); // Argumntos o los valores reales
+sumar(3,3);
+sumar(3,6);
+sumar(1);
+
+const sumar2 = function(n1, n2){
+    console.log(n1 + n2);
+}
+sumar2(5,10);
