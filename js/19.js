21f2c0b1759f0921c8e859d272252bd376259936
Adolfo20bm <abm2491@gmail.com>
2023-05-16 07:06:00 -0500

    Funciones que retornan valores

---
+++ js/19.js
@@ -0,0 +1,23 @@
+//function sumar(n1, n2){
+//    return n1 + n2;
+//}
+//const resultado = sumar(2,3);
+
+let total = 0;
+
+function agregarCarrito(precio) {
+    return total+= precio;
+}
+function calcularImpuesto(total){
+    return 1.5*total;
+}
+
+total = agregarCarrito(200);
+total = agregarCarrito(400);
+total = agregarCarrito(600);
+
+console.log(total);
+
+const totalAPagar = calcularImpuesto(total);
+
+console.log(`El total a pagar con impuestos es de ${totalAPagar}`);
