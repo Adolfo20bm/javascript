39746dbbea8590ca435787351a97a3024264b6db
Adolfo20bm <abm2491@gmail.com>
2023-05-16 06:36:23 -0500

    Funciones en JS

---
+++ js/16.js
@@ -0,0 +1,23 @@
+//DECLARACION DE FUNCION
+
+function sumar() {
+    console.log(10 * 10);
+}
+
+sumar()
+
+//Expresion de la funcion
+const sumar2 = function(){
+    console.log(3 + 3 );
+}
+sumar2();
+
+//IIFE
+(function(){
+    console.log('Esto es una funcion');
+})();
+
+//se puede llamar desde otro archivo crea un js con nombre otro y define el const cliente = 'Juan'; y lo podra llamar
+//console.log(cliente);
+
+
