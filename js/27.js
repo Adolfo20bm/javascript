694997a1cf9bbda6661504679e73be702ee383ba
Adolfo20bm <abm2491@gmail.com>
2023-05-17 12:41:28 -0500

    Prototypes

--- js/27.js
+++ js/27.js
@@ -8,14 +8,29 @@ const producto = {
 
 // Object Constructor
 //La ventaja es que puedes crear pmultiples objetos y varables
+function Cliente(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+Cliente.prototype.formatearCliente = function() {
+    return `El Cliente ${this.nombre} ${this.apellido}`;
+}
 
 function Producto(nombre, precio) {
     this.nombre = nombre;
     this.precio = precio;
-
 }
+//crear funcioness que solo se utilizan en un objeto mas en especeifico
+Producto.prototype.formatearProducto = function() {
+    return `El Producto ${this.nombre} tiene un precio de: $ ${this.precio}`;
+}
+
 const producto2 = new Producto('Monitor Curvo de 49"', 800);
 const producto3 = new Producto('Laptop"', 300);
+const cliente = new Cliente('Adolfo','Morales');
 
-console.log(producto2);
-console.log(producto3);
+console.log(producto2)
+console.log(producto2.formatearProducto());
+console.log(producto3.formatearProducto());
+console.log(cliente.formatearCliente());
+
