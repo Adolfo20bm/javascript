2487391d77522b6dd6602bd4432eb59aa5558617
Adolfo20bm <abm2491@gmail.com>
2023-05-17 12:22:47 -0500

    Object Constructor y Object Literal 1

---
+++ js/27.js
@@ -0,0 +1,21 @@
+// POO
+
+/** Object Literal*/
+const producto = {
+    nombre: 'Tablet',
+    precio: 500
+}
+
+// Object Constructor
+//La ventaja es que puedes crear pmultiples objetos y varables
+
+function Producto(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+
+}
+const producto2 = new Producto('Monitor Curvo de 49"', 800);
+const producto3 = new Producto('Laptop"', 300);
+
+console.log(producto2);
+console.log(producto3);
