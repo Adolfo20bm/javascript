0d0ce89896b68d5d31ce49de76dcd290b7ef3f3e
Adolfo20bm <abm2491@gmail.com>
2023-05-19 13:23:13 -0500

    como trabajar con dos consultas Async y Await

--- js/32.js
+++ js/32.js
@@ -10,10 +10,26 @@ function descargarNuevosClientes(){
     });
 }
 
+function descargarUltimosPedidos(){
+    return new Promise( resolve => {
+        console.log('Descargando pedidos... espere...');
+
+        setTimeout(()=>{
+            resolve('Los pedidos fueron descargados');
+        }, 3000 );
+    });
+}
+
 async function app() {
     try {
-        const resultado = await descargarNuevosClientes();
-        console.log(resultado);
+        // const clientes = await descargar Nuevos Clintes();
+        // const pedidos = await descargarUltimos pedidos();
+        // console.log(clientes);
+        // console.log(pedidos);
+
+        const resultado = await Promise.all([descargarNuevosClientes(), descargarUltimosPedidos()]);
+        console.log(resultado[0]);
+        console.log(resultado[1]);
     } catch(error){
         console.log(error);
     }
