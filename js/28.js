e0437b5a2468eaabcc97742b0c9e601b05b61c6d
Adolfo20bm <abm2491@gmail.com>
2023-05-19 11:57:03 -0500

    POO Herencias

--- js/28.js
+++ js/28.js
@@ -14,8 +14,21 @@ class Producto {
 const producto2 = new Producto('Monitor Curvo de 49"', 800);
 const producto3 = new Producto('Laptop"', 300);
 
+
+//HERENCIA
+
+class Libro extends Producto {
+    constructor(nombre,precio, isbn){
+        super(nombre, precio);
+        this.isbn = isbn;
+    }
+
+    formatearProducto(){
+        return `${super.formatearProducto()} y su ISBM es ${this.isbn}`;
+    }
+}
+
+const libro = new Libro('JavaScript la revolucion', 120, '912456987456654');
+console.log(libro.formatearProducto());
 console.log(producto2.formatearProducto());
-console.log(producto3.formatearProducto());
-
-
 
